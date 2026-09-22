bf08e144dc5bbdd1b780ff618bace33a7d8771ff
Devarakonda Harsha Vardan <109481657+cipyout31@users.noreply.github.com>
2026-09-22 18:31:03 +0530

    Change console output and return value in cipy method

--- src/main/java/com/example/demo/harsha.java
+++ src/main/java/com/example/demo/harsha.java
@@ -8,7 +8,7 @@ public class harsha {
 
     @GetMapping("/")
     public String cipy() {
-        System.out.println("cipy printed to console final");
-        return "Now it is working fine anf dng"; // This text will now display in the browser
+        System.out.println("sleep");
+        return "sleep"; // This text will now display in the browser
     }
 }
